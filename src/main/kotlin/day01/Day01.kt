dd59a14aa1ad8feb88d9bd7b387229437eaac37b
Omkar Konaraddi <19355185+konaraddi@users.noreply.github.com>
2025-12-17 00:05:54 -0500

    Fix Day 1 part 2 edge case for landing on position 0

--- src/main/kotlin/day01/Day01.kt
+++ src/main/kotlin/day01/Day01.kt
@@ -1,11 +1,15 @@
 package day01
 
 import utils.readInput
+import kotlin.math.abs
 
 fun main() {
     val lines = readInput(1)
     val answer = part1(lines)
-    println("day 1: $answer")
+    println("day 1 part 1: $answer")
+
+    val part2answer = part2(lines)
+    println("day 1 part 2: $part2answer")
 }
 
 fun part1(lines: List<String>): Int {
@@ -24,10 +28,41 @@ fun part1(lines: List<String>): Int {
 
         position = position.mod(100)
 
-        when {
-            position < 0 -> position += 100
-            position > 99 -> position -= 100
-            position == 0 -> count++
+        if (position == 0) {
+            count++
+        }
+    }
+
+    return count
+}
+
+fun part2(lines: List<String>): Int {
+    var count = 0
+    var position = 50
+
+    for (line in lines) {
+        if (line.isBlank()) {
+            continue
+        }
+
+        val dialDirectionIsReverse = line[0] == 'L'
+        val dialMovement = line.substring(1).toInt()
+
+        val originalPosition = position
+        position = if (dialDirectionIsReverse) position - dialMovement else position + dialMovement
+
+        count += dialMovement / 100
+
+        position = position.mod(100)
+
+        if (originalPosition == 0) {
+            continue
+        }
+
+        if (position == 0 ||
+            (dialDirectionIsReverse && position > originalPosition) ||
+            (!dialDirectionIsReverse && position < originalPosition)) {
+            count++
         }
     }
 
--- src/test/kotlin/day01/Day01Test.kt
+++ src/test/kotlin/day01/Day01Test.kt
@@ -15,8 +15,31 @@ class Day01Test {
         L100
     """.trimIndent().lines()
 
+    private val testInput2 = """
+        R50
+        L301
+        R402
+    """.trimIndent().lines()
+
+    private val testInput3 = """
+        L51
+    """.trimIndent().lines()
+
+    private val testInput4 = """
+        R50
+        L101
+    """.trimIndent().lines()
+
     @Test
-    fun `testInput1 part1`() {
+    fun part1() {
         assertEquals(2, part1(testInput1))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(2, part2(testInput1))
+        assertEquals(9, part2(testInput2))
+        assertEquals(1, part2(testInput3))
+        assertEquals(2, part2(testInput4))
     }
 }
